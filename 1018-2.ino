9ab35d5df5d2d9d73b35bc409bcfb8523437c60d
Liliette <1234lilian1234@gmail.com>
2017-10-18 11:59:27 +0800

    Add files via upload

---
+++ 1018-2.ino
@@ -0,0 +1,24 @@
+const int ledCount = 10;
+int ledPins[] = {2,3,4,5,6,7,8,9};
+#define fsr_pin A0
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  for(int thisLed = 0;thisLed < ledCount;thisLed++){
+    pinMode(ledPins[thisLed],OUTPUT);
+    }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int fsr_value = analogRead(fsr_pin);
+  int led_value = map(fsr_value,0,1023,0,255);
+  for(int thisLed = 0; thisLed < ledCount; thisLed++){
+    if(thisLed < led_value){
+      digitalWrite(ledPins[thisLed],HIGH);
+      }
+    else{
+      digitalWrite(ledPins[thisLed],LOW);
+      }
+    }
+}
